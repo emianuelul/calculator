04b95b0c3a490d812359c7463af8d7ec65dc2c2f
emianuelul <emanuel.marin0000@gmail.com>
2025-06-06 01:48:44 +0300

    feat: can now compute different formulas, not with floats tho

--- code.js
+++ code.js
@@ -5,8 +5,12 @@ let buttonList;
 const resultBar = document.querySelector('.result');
 
 const operations = ['AC','+', '-', '*', '/', '='];
-
-
+const priority = {
+    '+':1,
+    '-':1,
+    '*':2,
+    '/':2,
+}
 
 function initializeButtons(){
     for(let i = 9; i >= -1; i--){
@@ -26,7 +30,6 @@ function initializeButtons(){
         button.id = `${operations[i]}-key`;
         button.classList.add(`blur`);
         if(operations[i] === '='){
-            console.log('ok');
             button.style.backgroundColor = 'rgba(59, 102, 194, 0.83)';
         }
         button.textContent = operations[i];
@@ -41,26 +44,128 @@ function clearScreen(){
     resultBar.textContent = '';
 }
 
+function isDigit(a){
+    return a >= '0' && a <= '9';
+}
+
+function postfix(formula){
+    let postfixat = [];
+    let operators = [];
+    for(const item of formula){
+        if(typeof item === 'number'){
+            postfixat.push(item);
+        } else {
+            while (
+                operators.length > 0 &&
+                priority[item] <= priority[operators[operators.length - 1]]
+            ) {
+                postfixat.push(operators.pop());
+            }
+            operators.push(item);
+        }
+    }
+
+    while(operators.length > 0){
+        postfixat.push(operators.pop());
+    }
+
+    return postfixat;
+}
+
 function computeFormula(input){
-    let formula = input.split('+');
+    // build the formula
+    let formula = [];
+    for(let i = 0; i < input.length; i++){
+        let number = new String();
+
+        if((i === 0 || operations.includes(input[i-1])) && input[i] === '-'){
+            number += '-'
+            i++;
+        }
+        while(isDigit(input[i]) && i < input.length){
+            number += input[i];
+            i++;
+        }
+        formula.push(parseInt(number));
+        
+        formula.push(input[i]);
+    }
+
+    formula.pop();
     console.log(formula);
+
+    formula = postfix(formula);
+    
+    // compute the formula
+    let result = [];
+    for(const item of formula){
+        if(typeof item === 'number'){
+            result.push(item);
+        }
+        else{
+            let b = result.pop();
+            let a = result.pop();
+
+            switch(item){
+                case '+':{
+                    result.push(a+b);
+                    break;
+                }
+                case '-':{
+                    result.push(a-b);
+                    break;
+                }
+                case '*':{
+                    result.push(a*b);
+                    break;
+                }
+                case '/':{
+                    result.push(a/b);
+                    break;
+                }
+            }
+        }
+    }
+    return result.pop();
 }
 
 buttonList.forEach(element => {
     element.addEventListener('click', (event) => {
-        if((element.id[0] >= '0' && element.id[0] <='9') || (operations.includes(element.id[0]) && element.id[0] !== '=') ||
-            element.id[0] === '.'){
-            resultBar.textContent += element.id[0];
-            resultBar.scrollLeft = resultBar.scrollWidth;
+        const value = element.id[0];
+        const text = resultBar.textContent;
+        const lastChar = text[text.length - 1];
+        const secondLast = text[text.length - 2];
+
+        if(value >= '0' && value <= '9'){
+            resultBar.textContent += value;
         }
-        else if(element.id[0] === '='){
-            computeFormula(resultBar.textContent);
+        else if(operations.includes(value) && value !== '='){
+            if(text.length === 0 && value !== '-')
+                return;
+
+            if(value === '-'){
+                if(!(lastChar === '-' && secondLast === '-')){
+                    resultBar.textContent += value;
+                }
+            }
+            else{
+                if(!operations.includes(lastChar)){
+                    resultBar.textContent += value;
+                }
+            }
         }
-        else if(element.id[0] === 'A'){
+        else if(value === '='){
+            resultBar.textContent = computeFormula(text);
+        }
+        else if(value === 'A'){
             clearScreen();
         }
-    })
+
+        resultBar.scrollLeft = resultBar.scrollWidth;
+
+    });
 });
+
 
 
 
